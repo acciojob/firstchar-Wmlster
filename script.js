9d54b26469ab5e28b344f817dc037478267bbacc
Manik Sharma <90952302+Wmlster@users.noreply.github.com>
2023-11-19 15:58:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function firstChar(text) {
   // your code here
+
+	let s="";
+
+	for(let i=0;i<text.length;i++)
+		{
+			if(text[i]!=" ")
+			{
+				s+=text[i];
+				break;
+			}
+		}
+
+	return s;
 }
 
 // Do not change the code below
